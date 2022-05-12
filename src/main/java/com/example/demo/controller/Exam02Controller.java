5bc4c43f0757cbbd94c101d8a78086db5ecee56a
hayate-sato <hayate.sato@rakus-partners.co.jp>
2022-05-12 11:52:50 +0900

    2番終了

--- src/main/java/com/example/demo/controller/Exam02Controller.java
+++ src/main/java/com/example/demo/controller/Exam02Controller.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.Exam02Form;
 
@@ -13,7 +14,8 @@ import com.example.demo.Exam02Form;
 @RequestMapping("/exam02")
 public class Exam02Controller {
 
-	
+	@Autowired
+	private HttpSession session;
 	
 	@ModelAttribute
 	public Exam02Form setUpForm() {
@@ -25,24 +27,31 @@ public class Exam02Controller {
 	}
 	
 	@RequestMapping("/receive")
-	public String calc (@Validated Exam02Form exam02Form, BindingResult result,
-			RedirectAttributes redirectAttribute) {
-		if (result.hasErrors()) {
-			return index();
-		}else {
+	public String calc(@Validated Exam02Form exam02Form) {
+
 			Integer num1=
 					Integer.parseInt(exam02Form.getNum1());
 			Integer num2=
 					Integer.parseInt(exam02Form.getNum2());
 			Integer answer = num1+num2;
 			
-			redirectAttribute.addFlashAttribute("answer",answer);
+			session.setAttribute("num1", num1);
+			session.setAttribute("num2", num2);
+			session.setAttribute("answer", answer);
+
 			return "redirect:/exam02/toresult";
+
 		}
-	}
 	
+
 	@RequestMapping("/toresult")
 	public String toResult() {
 		return "exam02-result";
 	}
+
+	@RequestMapping("/to-page1")
+	public String toPage1() {
+		return "exam02-result2";
+	}
 }
+
